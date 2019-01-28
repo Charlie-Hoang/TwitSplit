8e5b9c82d157e36769968d7e479ec6cd6b97f71a
Charlie <chickchick.ce@gmail.com>
2019-01-28 17:22:49 +0700

    inital project

---
+++ Podfile
@@ -0,0 +1,24 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+workspace 'TwitSplit.xcworkspace'
+
+use_frameworks!
+
+inhibit_all_warnings!
+
+def framework_pods
+	pod 'RxSwift'
+	pod 'RxCocoa'
+end
+
+target 'TwitSplitApp' do
+	project 'TwitSplitApp/TwitSplitApp.xcodeproj'
+    framework_pods
+end
+
+target 'TwitSplitCore' do
+	project 'TwitSplitCore/TwitSplitCore.xcodeproj'
+    framework_pods
+end
+
---
+++ TwitSplitCore/TwitSplitCore/TwitSplitCore.h
@@ -0,0 +1,19 @@
+//
+//  TwitSplitCore.h
+//  TwitSplitCore
+//
+//  Created by Charlie on 1/28/19.
+//  Copyright © 2019 CuongHH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TwitSplitCore.
+FOUNDATION_EXPORT double TwitSplitCoreVersionNumber;
+
+//! Project version string for TwitSplitCore.
+FOUNDATION_EXPORT const unsigned char TwitSplitCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TwitSplitCore/PublicHeader.h>
+
+
